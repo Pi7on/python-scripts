3c08bfcbdb40cd87133fb68c2994c1478069f509
Pi7on <45372614+Pi7on@users.noreply.github.com>
2022-01-25 00:41:13 +0100

    add "esiste-parola"

---
+++ esiste-parola.py
@@ -0,0 +1,12 @@
+import requests as r
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('parola', type=str)
+args = parser.parse_args()
+
+url = "https://www.treccani.it/vocabolario/ricerca/"+args.parola+"/"
+response = r.get(url)
+print(("SI" if response.status_code == 200 else "NO"))
+sys.exit((1 if response.status_code == 200 else 0))
